fa8d91c1bab42278a02fab8af323203a58141f1a
mmmatthew <sb#z@A&gB99g>
2015-08-25 16:51:21 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name polhydroApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the polhydroApp
+ */
+angular.module('polhydroApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
